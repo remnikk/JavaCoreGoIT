9bb6cdf5a7638d2f23386931ec75d9a7dc88aedf
remnikk <remnik@ua.fm>
2016-08-24 21:06:55 +0300

    First Module

---
+++ FirstClass.java
@@ -0,0 +1,8 @@
+package Module1;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+
+    }
+}
---
+++ FourthClass.java
@@ -0,0 +1,7 @@
+package Module1;
+
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
